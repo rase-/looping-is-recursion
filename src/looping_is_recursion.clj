6e22c0b51d1e0c1241f2f46db99997978a1273a0
Tony E J Kovanen <tonykova@e42-19.cs.helsinki.fi>
2012-10-24 12:46:15 +0300

    Some done

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,16 +1,34 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [helper (fn [base exp acc] (if (== 1 exp)
+                                    acc
+                                    (recur base (dec exp) (* acc base))))]
+    (if (== 0 exp)
+       1
+       (helper base exp base))))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [i a-seq] (if (== i (count a-seq))
+                               (get a-seq (dec i))
+                               (recur (inc i) a-seq)))]
+    (helper 0 a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (if (not (== (count seq1) (count seq2)))
+    false
+    (loop [i 0]
+      (cond
+       (== i (count seq1)) true
+       (not (= (first seq1) (first seq2))) false
+       :else (recur (inc i))))))
 
 (defn find-first-index [pred a-seq]
-  ":(")
+  (loop [i 0]
+    (cond 
+     (== i (count a-seq)) nil
+     (pred (get a-seq i)) i
+     :else (recur (inc i)))))
 
 (defn avg [a-seq]
   -1)
@@ -23,4 +41,3 @@
 
 (defn cut-at-repetition [a-seq]
   [":("])
-
