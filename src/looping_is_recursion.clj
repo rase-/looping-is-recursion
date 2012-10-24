d4d4224e011e10eb55d317e9ce38c9af06fe85db
Tony E J Kovanen <tonykova@e42-19.cs.helsinki.fi>
2012-10-24 14:34:29 +0300

    This batch done

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -31,13 +31,38 @@
      :else (recur (inc i)))))
 
 (defn avg [a-seq]
-  -1)
+  (loop [i 0
+         sum 0]
+    (if (== i (count a-seq))
+      (/ sum (count a-seq))
+      (recur (inc i) (+ sum (get a-seq i))))))
+
+(defn toggle [a-set elem]
+  (if (contains? a-set elem) (disj a-set elem) (conj a-set elem)))
 
 (defn parity [a-seq]
-  ":(")
+  (loop [i 0
+         s #{}]
+    (if (== i (count a-seq))
+      s
+      (recur (inc i) (toggle s (get a-seq i))))))
 
 (defn fast-fibo [n]
-  ":(")
+  (cond
+   (== n 0) 0
+   (== n 1) 1
+   :else (loop [fn-2 0
+                fn-1 1
+                i 2]
+           (if (== i n)
+             (+ fn-2 fn-1)
+             (recur fn-1 (+ fn-2 fn-1) (inc i))))))
 
 (defn cut-at-repetition [a-seq]
-  [":("])
+  (loop
+    [i 0
+     seen []]
+    (cond
+     (== i (count a-seq)) a-seq
+     (some (fn [x] (= x (get a-seq i))) seen) seen
+     :else (recur (inc i) (conj seen (get a-seq i))))))
